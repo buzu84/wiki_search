a4f1e4534cdff7c0a04c61fcb8322ff4399452b8
Magdalena Buzikiewicz <buzu@op.pl>
2020-10-16 15:40:39 +0200

    Sending API calls to wikipedia.

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,30 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component,  } from 'react'
 import './style.css'
 
 class Search extends Component {
   state = {
-    searchTerm: ""
+    searchTerm: "",
+    results: []
   }
 
   handleInputChange = e => {
-    this.setState({searchTerm: e.target.value});
+    const API = "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srlimit=5&origin=*&srsearch="
+    this.setState({searchTerm: e.target.value}, () => {
+      fetch(`${API}${this.state.searchTerm}`)
+        .then(response => response.json())
+        .then(response => this.setState({results: response.query.search}))
+        .catch(error => {
+          console.log("There was an error with fetching: ", error);
+        });
+    });
   };
 
 
   render () {
-    const { searchTerm } = this.state;
+    const { searchTerm, results } = this.state;
 
     return (
       <>
         <form className="form">
-          <input type="text"
-          name="searchTerm"
-          value={this.state.searchTerm}
-          onChange={this.handleInputChange}
+          <input
+            type="text"
+            name="searchTerm"
+            value={searchTerm}
+            onChange={this.handleInputChange}
           >
           </input>
         </form>
-        <div>{this.state.searchTerm}</div>
+        {results.map(element => {
+          return <span key={element.pageid}>{element.title}</span>
+        })}
       </>
     )
   }
